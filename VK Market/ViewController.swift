6cd3ac65ae23d585735fb7b8155fb959109c1a40
Алексей Лячко <lesha@leshas-macbook-pro.local>
2019-03-23 12:40:09 +0300

    Initial Commit

---
+++ VK Market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Market
+//
+//  Created by Lesha on 23/03/2019.
+//  Copyright © 2019 y7c4k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
